034c03cb9ff0ef5392bc736eb9d7c49cc5a6866c
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2021-01-07 14:51:28 -0800

    clean up

--- src/main/java/io/glossnyx/vibes/mixin/ScreenHandlerMixin.java
+++ src/main/java/io/glossnyx/vibes/mixin/ScreenHandlerMixin.java
@@ -7,24 +7,18 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
-
 @Mixin(ScreenHandler.class)
 class ScreenHandlerMixin {
-	@Shadow @Final public List<Slot> slots;
-
 	@Inject(method = "method_30010", at = @At("HEAD"), cancellable = true)
 	private void onRemoveStack(int slotIndex, int clickType, SlotActionType actionType, PlayerEntity player, CallbackInfoReturnable<ItemStack> cir) {
 		if (slotIndex < 0) return;
 
-		Slot slot = slots.get(slotIndex);
+		Slot slot = player.currentScreenHandler.slots.get(slotIndex);
 		if (slot == null) return;
 
 		ServerNetworking.INSTANCE.onQuickMove(slot, actionType, player);
